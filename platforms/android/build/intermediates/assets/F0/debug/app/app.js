34e01e5e1c1bb717300db5556e8ec8dde65d7f67
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2018-01-15 22:27:28 -0600

    Nada

--- platforms/android/build/intermediates/assets/F0/debug/app/app.js
+++ platforms/android/build/intermediates/assets/F0/debug/app/app.js
@@ -1,10 +1,9 @@
 /*
-In NativeScript, the app.js file is the entry point to your application.
-You can use this file to perform app-level initialization, but the primary
-purpose of the file is to pass control to the app’s first module.
+The app.js file is the entry point to your application.
 */
 
 require("./bundle-config");
+//require("./text/encoding/UTF_8");
 var application = require("application");
 
 application.start({ moduleName: "main-page" });
